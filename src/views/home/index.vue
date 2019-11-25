da4cd2a2d98123d4a25c03a640e6f17962e8ccbc
LJQ21 <2652214165@qq.com>
2019-11-25 20:37:09 +0800

    :bug: 修复首页底部导航遮挡商品信息

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,8 +1,9 @@
 <template lang='pug'>
   #home-container
-    van-nav-bar(title='校园二手商城')
-    van-sticky
-      van-search(placeholder='请输入搜索关键词' v-model='searchKey' @click='toSearch')
+    .home-header
+      van-nav-bar(title='校园二手商城')
+      van-sticky
+        van-search(placeholder='请输入搜索关键词' v-model='searchKey' @click='toSearch')
     // 顶部栏
       van-row.topbar
         van-col(span='2').topbar-tree
@@ -11,42 +12,44 @@
         van-col(sapn='22')
           van-search(placeholder='请输入搜索关键词' v-model='searchKey' shape='round')
     // 轮播图
-    van-swipe.swipe(:autoplay='3000')
-      van-swipe-item(v-for='(image, index) in images', :key='index')
-        img.swipe-image(v-lazy='image')
-    // 分类
-    van-grid(:column-num='3')
-      van-grid-item(icon='photo-o', text='四级')
-      van-grid-item(icon='photo-o', text='软考')
-      van-grid-item(icon='photo-o', text='二级')
-      van-grid-item(icon='photo-o', text='会计')
-      van-grid-item(icon='photo-o', text='教资')
-      van-grid-item(icon='ellipsis', text='分类' @click='toClassify')
-    van-divider 今日推荐
-    van-grid(:border='false', :column-num='3')
-      van-grid-item
-        van-image(src='https://grithink.cn/upload/2019/10/wallhaven-g8171e-ebc82640c6f542d39060251e23f45941.png')
-      van-grid-item
-        van-image(src='https://grithink.cn/upload/2019/10/wallhaven-qdowjr-6a99eed676c34b2084b69495d0020109.jpg')
-      van-grid-item
-        van-image(src='https://grithink.cn/upload/2019/10/wallhaven-392lk9-b28902d03a154aa58de4ac69d2f47174.jpg')
-    van-divider 最新
-    // 商品展示
-    van-card(num='2', price='2.00', desc='描述信息', title='商品标题', thumb='https://img.yzcdn.cn/vant/t-thirt.jpg')
-      div(slot='tags')
-        van-tag(plain='', type='danger') 标签
-        van-tag(plain='', type='danger') 标签
-    van-card(num='2', price='2.00', desc='描述信息', title='商品标题', thumb='https://img.yzcdn.cn/vant/t-thirt.jpg')
-      div(slot='tags')
-        van-tag(plain='', type='danger') 标签
-        van-tag(plain='', type='danger') 标签
-    van-card(num='2', price='2.00', desc='描述信息', title='商品标题', thumb='https://img.yzcdn.cn/vant/t-thirt.jpg')
-      div(slot='tags')
-        van-tag(plain='', type='danger') 标签
-        van-tag(plain='', type='danger') 标签
-    // van-sku(v-model='show' :sku='sku' :goods='goods')
-    // 底部导航
-    Tabbar(:tabNum='tabNum')
+    .home-content
+      van-swipe.swipe(:autoplay='3000')
+        van-swipe-item(v-for='(image, index) in images', :key='index')
+          img.swipe-image(v-lazy='image')
+      // 分类
+      van-grid(:column-num='3')
+        van-grid-item(icon='photo-o', text='四级')
+        van-grid-item(icon='photo-o', text='软考')
+        van-grid-item(icon='photo-o', text='二级')
+        van-grid-item(icon='photo-o', text='会计')
+        van-grid-item(icon='photo-o', text='教资')
+        van-grid-item(icon='ellipsis', text='分类' @click='toClassify')
+      van-divider 今日推荐
+      van-grid(:border='false', :column-num='3')
+        van-grid-item
+          van-image(src='https://grithink.cn/upload/2019/10/wallhaven-g8171e-ebc82640c6f542d39060251e23f45941.png')
+        van-grid-item
+          van-image(src='https://grithink.cn/upload/2019/10/wallhaven-qdowjr-6a99eed676c34b2084b69495d0020109.jpg')
+        van-grid-item
+          van-image(src='https://grithink.cn/upload/2019/10/wallhaven-392lk9-b28902d03a154aa58de4ac69d2f47174.jpg')
+      van-divider 最新
+      // 商品展示
+      van-card(num='2', price='2.00', desc='描述信息', title='商品标题', thumb='https://img.yzcdn.cn/vant/t-thirt.jpg')
+        div(slot='tags')
+          van-tag(plain='', type='danger') 标签
+          van-tag(plain='', type='danger') 标签
+      van-card(num='2', price='2.00', desc='描述信息', title='商品标题', thumb='https://img.yzcdn.cn/vant/t-thirt.jpg')
+        div(slot='tags')
+          van-tag(plain='', type='danger') 标签
+          van-tag(plain='', type='danger') 标签
+      van-card(num='2', price='2.00', desc='描述信息', title='商品标题', thumb='https://img.yzcdn.cn/vant/t-thirt.jpg')
+        div(slot='tags')
+          van-tag(plain='', type='danger') 标签
+          van-tag(plain='', type='danger') 标签
+      // van-sku(v-model='show' :sku='sku' :goods='goods')
+      // 底部导航
+    .home-btmnar
+      Tabbar(:tabNum='tabNum')
 </template>
 
 <script>
@@ -137,18 +140,12 @@ export default {
 
 <style lang="less" scoped>
 #home-container {
-  .topbar {
-    width: 100%;
-    .topbar-tree {
-      margin: 9px 0 0 10px;
-      &_font {
-        font-size: 10px;
+  .home-content {
+    margin-bottom: 50px;
+    .swipe {
+      .swipe-image {
+        max-width: 400px;
       }
-    }
-  }
-  .swipe {
-    .swipe-image {
-      max-width: 400px;
     }
   }
 }
